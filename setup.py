9d38e3f1448095a1a2a445da331e2de5499bf8d9
MrTomRod <roder.thomas@gmail.com>
2022-03-15 14:41:53 +0100

    bump version to 0.0.4

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ README = (HERE / 'README.md').read_text()
 # This call to setup() does all the work
 setup(
     name='opengenomebrowser-tools',
-    version='0.0.3',
+    version='0.0.4',
     description='Set of scripts to aid OpenGenomeBrowser administrators import data',
     long_description=README,
     long_description_content_type='text/markdown',
